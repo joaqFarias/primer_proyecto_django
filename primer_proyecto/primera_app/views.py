1635e6f5ebe4e66fe727f225ad909f07f7c2744c
Joaquin Farias <jfarias@lythium.cl>
2021-08-04 02:57:53 -0400

    commit inicial y ejercicio terminado

---
+++ primer_proyecto/primera_app/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render, HttpResponse, redirect, JsonResponse
+
+# Create your views here.
+def root(request):
+    return redirect('/blogs')
+
+def index(request):
+    return HttpResponse("placeholder para luego mostrar una lista de todos los blogs")
+
+def new(request):
+    return HttpResponse("placeholder para mostrar un nuevo formulario para crear un nuevo blog")
+
+def create(request):
+    return redirect('/')
+
+def show(request, number):
+    return HttpResponse(f"placeholder para mostrar el blog numero: {number}")
+
+def edit(request, number):
+    return HttpResponse(f"placeholder para editar el blog {number}")
+
+def destroy(request, number):
+    return redirect('/blogs')
+
+def http_json(request):
+    http_json = {'titulo': 'El titulo de la primera pagina'}
+    return JsonResponse(http_json)
